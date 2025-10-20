0d8cb94e86f72a1498c529fd06bf726c0d35e159
Guilherme Leite <guilherme.rolim@academico.ifpb.edu.br>
2025-10-20 15:38:16 -0300

    Organiza o projeto em pastas de versão

---
+++ 2_captura_obrigatoria/constantes.py
@@ -0,0 +1,13 @@
+"""
+Módulo que contém as constantes usadas no jogo de damas
+"""
+
+TAM_TAB = 8 
+CASA_VAZIA = " "  
+
+PECA_BRANCA = "●" 
+PECA_PRETA = "◉" 
+PECA_BRANCA_DAMA = "♚"  
+PECA_PRETA_DAMA = "♔"  
+
+EMPATE = "empate"
